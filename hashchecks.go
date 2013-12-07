0971407132b730ab1356b273cdc637a096fde8ae
Toon Schoenmakers <nighteyes1993@gmail.com>
2013-12-07 20:53:44 +0100

    Added in support for sha1, sha256 and sha512
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

--- hashchecks.go
+++ hashchecks.go
@@ -19,6 +19,9 @@ package main
 
 import (
 	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/hex"
 	"flag"
 	"fmt"
@@ -52,18 +55,54 @@ func (t *nilSafeMultiWriter) Write(p []byte) (n int, err error) {
 }
 
 var md5hash *string = flag.String("md5", "", "A md5 hash to check against")
+var sha1hash *string = flag.String("sha1", "", "A sha1 hash to check against")
+var sha256hash *string = flag.String("sha256", "", "A sha256 hash to check against")
+var sha512hash *string = flag.String("sha512", "", "A sha512 hash to check against")
 
 func checkhashes(input io.Reader) (fails uint8) {
 	var md5writer hash.Hash = nil
 	if *md5hash != "" {
 		md5writer = md5.New()
 	}
-	hashwriter := NilSafeMultiWriter(md5writer)
+	var sha1writer hash.Hash = nil
+	if *sha1hash != "" {
+		sha1writer = sha1.New()
+	}
+	var sha256writer hash.Hash = nil
+	if *sha256hash != "" {
+		sha256writer = sha256.New()
+	}
+	var sha512writer hash.Hash = nil
+	if *sha512hash != "" {
+		sha512writer = sha512.New()
+	}
+	hashwriter := NilSafeMultiWriter(md5writer, sha1writer, sha256writer, sha512writer)
 	io.Copy(hashwriter, input)
 	if md5writer != nil {
 		md5output := hex.EncodeToString(md5writer.Sum(nil))
 		if md5output != *md5hash {
-			fmt.Printf("md5 mismatch, expected: %s. Got: %s\n", *md5hash, md5output)
+			fmt.Printf("md5 mismatch, expected: %s, got: %s\n", *md5hash, md5output)
+			fails++
+		}
+	}
+	if sha1writer != nil {
+		sha1output := hex.EncodeToString(sha1writer.Sum(nil))
+		if sha1output != *sha1hash {
+			fmt.Printf("sha1 mismatch, expected: %s, got: %s\n", *sha1hash, sha1output)
+			fails++
+		}
+	}
+	if sha256writer != nil {
+		sha256output := hex.EncodeToString(sha256writer.Sum(nil))
+		if sha256output != *sha256hash {
+			fmt.Printf("sha256 mismatch, expected: %s, got: %s\n", *sha256hash, sha256output)
+			fails++
+		}
+	}
+	if sha512writer != nil {
+		sha512output := hex.EncodeToString(sha512writer.Sum(nil))
+		if sha512output != *sha512hash {
+			fmt.Printf("sha512 mismatch, expected: %s, got: %s\n", *sha512hash, sha512output)
 			fails++
 		}
 	}
